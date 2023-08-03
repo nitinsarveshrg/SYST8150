c0bdfe7d5efed6e1aca0b244ce768cb165c8d755
nitinsarveshrg <141292370+nitinsarveshrg@users.noreply.github.com>
2023-08-02 22:34:27 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "nitinsarveshraajagopal"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
