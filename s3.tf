52726fc7819fe688c98dc1b9fc61b4a46b8919a2
nitinsarveshrg <141292370+nitinsarveshrg@users.noreply.github.com>
2023-08-03 00:11:44 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,8 +2,8 @@ resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
-   versioning {
-    enabled = var.versioning
+   versioning_configuration {
+    status = "Enabled"
   }
   
   tags = var.tags
