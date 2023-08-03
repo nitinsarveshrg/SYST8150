c24821a89ce22bd23f891f93e173bfba1652cad8
KARTIK SAXENA <46376692+kartiks2000@users.noreply.github.com>
2023-08-03 13:11:21 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
        default     = "us-east-1" 
 }
 
-variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
-}
-
 variable "instance_type" { 
     description = "instance type for ec2" 
     default     =  "t2.micro" 
